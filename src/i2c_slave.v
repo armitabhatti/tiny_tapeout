41fb8ce0fd9aaf048ff1122c88c2d75071a02efb
armita <155481221+armitabhatti@users.noreply.github.com>
2025-12-01 13:01:16 -0500

    Update i2c_slave.v

--- src/i2c_slave.v
+++ src/i2c_slave.v
@@ -147,7 +147,7 @@ module i2c_slave #(
 
                     if (bit_counter == 7) begin
                         //addr_rw       <= ({shreg[6:0], sda_in} >> 1)[6:0];
-                        address_match <= ( {shreg[6:0], sda_in} >> 1 )[6:0] == I2C_ADDR;
+                        address_match <= ( {shreg[6:0], sda_in} >> 1 )== {1'b0, I2C_ADDR};
                         read_check    <= sda_in;
                         bit_counter <= 0;
                         ack_asserted <= 0;
